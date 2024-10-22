3f78089a790eabe1d1bb130ceea725730324ee3e
namthse182380 <namthse182380@fpt.edu.vn>
2024-10-22 12:00:29 +0700

    Add files via upload

--- deepfake_detection/settings.py
+++ deepfake_detection/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x(orck$z%ijba%&k^ib@9e0c9py-dsm!i%4=v-cglt+scjq6xn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
